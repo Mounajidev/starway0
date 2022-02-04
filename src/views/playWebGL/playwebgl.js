068554010258032f3de52d87d77d5f95a61bb2b8
Mounajidev <lucilo@gmail.com>
2022-02-04 16:23:44 -0300

    423

--- src/views/playWebGL/playwebgl.js
+++ src/views/playWebGL/playwebgl.js
@@ -1,87 +1,87 @@
-import useMounajiTestContract from "../../hooks/useMounajiTestContract";
-import {
-  Stack,
-  Flex,
-  Heading,
-  Text,
-  Box,
-  Button,
-  Image,
-  Badge,
-  useToast
-} from "@chakra-ui/react";
-import { Link } from "react-router-dom";
-import { useWeb3React } from "@web3-react/core";
+// import useMounajiTestContract from "../../hooks/useMounajiTestContract";
+// import {
+//   Stack,
+//   Flex,
+//   Heading,
+//   Text,
+//   Box,
+//   Button,
+//   Image,
+//   Badge,
+//   useToast
+// } from "@chakra-ui/react";
+// import { Link } from "react-router-dom";
+// import { useWeb3React } from "@web3-react/core";
 
-import { useCallback, useEffect, useState } from "react";
-import Unity, { UnityContent, UnityContext } from "react-unity-webgl";
+// import { useCallback, useEffect, useState } from "react";
+// import Unity, { UnityContent, UnityContext } from "react-unity-webgl";
 
 
 
-const PlayWebGL = () => {
+// const PlayWebGL = () => {
     
-    const [isMinting, setIsMinting] = useState(false);
-    const [imageSrc, setImageSrc] = useState("");
-    const { active, account } = useWeb3React();
-    const mounajiTestContract = useMounajiTestContract();
-    const toast = useToast();
+//     const [isMinting, setIsMinting] = useState(false);
+//     const [imageSrc, setImageSrc] = useState("");
+//     const { active, account } = useWeb3React();
+//     const mounajiTestContract = useMounajiTestContract();
+//     const toast = useToast();
 
-    const unityContent = new UnityContent(
-      './Build/WebGL.json',
-      './Build/UnityLoader.js'
-    )
-    unityContent.on("quitted", () => {
-      console.log('Game quit')
-    })
-    unityContent.on("loaded", () => {
-      console.log('Game loaded')
-    })
-    unityContent.on("progress", progression => {
-      console.log('Game loading', progression)
-    })
-    unityContent.on("error", message => {
-      console.log('Game errored', message)
-    })
-    unityContent.on("DemoUnityToReact", (params) => {
-      console.log('DemoUnityToReact', params)
-    })
+//     const unityContent = new UnityContent(
+//       './Build/WebGL.json',
+//       './Build/UnityLoader.js'
+//     )
+//     unityContent.on("quitted", () => {
+//       console.log('Game quit')
+//     })
+//     unityContent.on("loaded", () => {
+//       console.log('Game loaded')
+//     })
+//     unityContent.on("progress", progression => {
+//       console.log('Game loading', progression)
+//     })
+//     unityContent.on("error", message => {
+//       console.log('Game errored', message)
+//     })
+//     unityContent.on("DemoUnityToReact", (params) => {
+//       console.log('DemoUnityToReact', params)
+//     })
          
     
-    return (
+//     return (
         
-    <Box
-    maxW='fixed' borderWidth='1px' borderRadius='lg'
-    // backgroundImage="'/images/fondoespacio.png'"
-    image="./images.starway.png"
-    pos={"absolute"}
-    align={"center"}
-    justify={"center"}
-    backgroundRepeat={"no-repeat"}
-    backgroundSize={"cover"}
-    margin={"0"}
-    top={"55px" }
-    left={"0"}
-    w={"100%"}
-    h={"100%"}
+//     <Box
+//     maxW='fixed' borderWidth='1px' borderRadius='lg'
+//     // backgroundImage="'/images/fondoespacio.png'"
+//     image="./images.starway.png"
+//     pos={"absolute"}
+//     align={"center"}
+//     justify={"center"}
+//     backgroundRepeat={"no-repeat"}
+//     backgroundSize={"cover"}
+//     margin={"0"}
+//     top={"55px" }
+//     left={"0"}
+//     w={"100%"}
+//     h={"100%"}
     
-  >
-    <Stack marginTop={'20px'}>
+//   >
+//     <Stack marginTop={'20px'}>
       
-    <h1>Play Starway from the web browser !</h1>
-    </Stack>
+//     <h1>Play Starway from the web browser !</h1>
+//     </Stack>
 
-    <Unity unityContent={unityContent} width="100%" height="100%" />  
+//     <Unity unityContent={unityContent} width="100%" height="100%" />  
       
     
-       </Box>  
+//        </Box>  
         
       
-    );
-  };
+//     );
+//   };
 
 
 
 
 
 
-export default PlayWebGL;
+// export default PlayWebGL;
